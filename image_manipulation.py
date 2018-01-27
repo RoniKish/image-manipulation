1205cc53dd2fb4433d7981d027aa2a4d560598eb
RoniKish <abc@example.com>
2018-01-27 18:15:10 +0200

    added reflection option

--- image_manipulation.py
+++ image_manipulation.py
@@ -12,7 +12,8 @@ def interface():
         2: resize_image,
         3: duplicate_image,
         4: diverse_image,
-        5: save_image
+        5: reflect_image,
+        6: save_image
     }
     pick = 1
     # I used test_1.png,test_2.png as names
@@ -28,32 +29,35 @@ def interface():
         print('2 to resize the image')
         print('3 to multiply the image')
         print('4 to diverse the image')
-        print('5 to save the image')
-        print('6 to pick another image')
+        print('5 to reflect the image')
+        print('6 to save the image')
+        print('7 to pick another image')
         pick = int(input('your input: '))
         # In case the user wish to exit
         if pick == 0:
             break
         # In case the user wish to change the image
-        if pick == 6:
+        if pick == 7:
+            if os.path.isfile('result.png'):
+                os.remove('result.png')
             path = input('Enter the file name: ')
             continue
         # In case the pick is out of bounds
-        if pick < 0 or pick > 6:
+        if pick < 0 or pick > 7:
             print('You must select a number between 0 to 2')
             continue
         menu[pick](path)
 
 
 def duplicate_image(path):
-    # Takes the number of times the user wish to duplicate the image
-    runs = int(input('Enter the number of times u wish to multiply the image: '))
     # If the image was already changed open the changed version, other wise create a result file
     if os.path.isfile('result.png'):
         image = Image.open('result.png')
     else:
         image = Image.open(path)
         image.save('result.png')
+    # Takes the number of times the user wish to duplicate the image
+    runs = int(input('Enter the number of times u wish to multiply the image: '))
     # Duplicates the image the number of times we want
     for i in range(runs):
         image = Image.open('result.png')
@@ -74,15 +78,15 @@ def duplicate_image(path):
 
 
 def resize_image(path):
-    # Enter the width and height we wish to change
-    width = int(input('Enter the width of the image: '))
-    height = int(input('Enter the height of the image: '))
     # If the image was already changed open the changed version, other wise create a result file
     if os.path.isfile('result.png'):
         image = Image.open('result.png')
     else:
         image = Image.open(path)
         image.save('result.png')
+    # Enter the width and height we wish to change
+    width = int(input('Enter the width of the image: '))
+    height = int(input('Enter the height of the image: '))
     # Creates and saves the image
     new_image = image.resize((width, height))
     new_image.save('result.png')
@@ -97,14 +101,14 @@ def show_image(path):
 
 
 def diverse_image(path):
-    # Takes the number of times the user wish to diverse the image
-    runs = int(input('Enter the number of times u wish to diverse the image: '))
     # If the image was already changed open the changed version, other wise create a result file
     if os.path.isfile('result.png'):
         image = Image.open('result.png')
     else:
         image = Image.open(path)
         image.save('result.png')
+    # Takes the number of times the user wish to diverse the image
+    runs = int(input('Enter the number of times u wish to diverse the image: '))
     # Diverse the image the number of times we want
     for i in range(runs):
         image = Image.open('result.png')
@@ -124,6 +128,36 @@ def save_image(path):
     name = input('Enter the image name: ')
     name = name + '.png'
     image.save(name)
+
+
+def reflect_image(path):
+    # If the image was already changed open the changed version, other wise create a result file
+    if os.path.isfile('result.png'):
+        image = Image.open('result.png')
+    else:
+        image = Image.open(path)
+        image.save('result.png')
+    # Decides on the reflection angle
+    print('Enter 1 for up-down reflect')
+    print('Enter 2 for left-right reflect')
+    choice = int(input('Your input'))
+    while choice < 1 or choice > 2:
+        choice = int(input('You must choose 1 or 2'))
+    if choice == 1:
+        width, height = image.size
+        box = (0, 0, width, (height * 2))
+        cropped_image = image.crop(box)
+        image_reflect = image.transpose(Image.FLIP_TOP_BOTTOM)
+        cropped_image.paste(image_reflect, (0, height, width, (height * 2)))
+    if choice == 2:
+        width, height = image.size
+        box = (0, 0, (width * 2), height)
+        cropped_image = image.crop(box)
+        image_reflect = image.transpose(Image.FLIP_LEFT_RIGHT)
+        cropped_image.paste(image_reflect, (width, 0, (width * 2), height))
+
+    cropped_image.save('result.png')
+
 
 
 interface()
